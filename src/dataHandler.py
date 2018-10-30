masking out known values, visdom correct implemented, many implementation improvements, fixed log_transform

src/dataHandler.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import h5py
+import os
 from torchvision import transforms
 from torch.utils.data.dataset import Dataset
 from torch.utils.data.sampler import SubsetRandomSampler
@@ -8,10 +9,16 @@ from torch.utils.data.sampler import SubsetRandomSampler
 class DataHandler(Dataset):
   def __init__(self, path, truncation=3):
     super(DataHandler, self).__init__()
-    file = h5py.File(path, 'r')
-    self.data = file['data']
-    self.target = file['target']
     self.truncation = truncation
+    self.data = []
+    self.target = []
+
+    for root, dirs, files in os.walk(path):
+      for file in files:
+        if file.endswith('.h5'):
+          h5_file = h5py.File(os.path.join(root,file), 'r')
+          self.data = np.array(h5_file['data'])
+          self.target = np.array(h5_file['target'])
 
   def __getitem__(self, key):
     if isinstance(key, slice):
@@ -33,12 +40,12 @@ class DataHandler(Dataset):
 
   def get_item_from_index(self, index):
     to_tensor = transforms.ToTensor()
-    vol = torch.from_numpy(self.data[index]).float()
-    vol[0].abs_().clamp_(max=self.truncation)
-    vol.requires_grad_()
+    tsdf = torch.from_numpy(self.data[index]).float()
+    tsdf[0].abs_().clamp_(max=self.truncation)
+    tsdf.requires_grad_()
     target = torch.from_numpy(self.target[index]).clamp(max=self.truncation).float()
 
-    return vol, target
+    return tsdf, target
 
   def subdivide_dataset(self, val_size, shuffle=False, seed=1):
     num_samples = int(len(self))

src/demo.py
@@ -12,18 +12,21 @@ from skimage import measure
 from mpl_toolkits.mplot3d import Axes3D
 from nn import MyNet
 
-parser = argparse.ArgumentParser(description='Demo')
-parser.add_argument('--model', type=str, default='../models/checkpoint.pth',
-                    help='Trained model path')
-parser.add_argument('--file', type=str, default='../datasets/sample/overfit.h5',
-                    help='Use file as input')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='disables CUDA')
-args = parser.parse_args()
-args.cuda = not args.no_cuda and torch.cuda.is_available()
+def main(argmodel, argfile, argcuda=False, show=False, timeit=False):
+  model = MyNet()
+  device = torch.device("cuda:0" if argcuda else "cpu")
 
-def predict_and_plot(inputs, model, target=None, timeit=False):
-  model.eval()
+  model.load_state_dict(torch.load(argmodel)['state_dict'])
+  model.to(device).eval()
+
+  file = h5py.File(argfile, 'r')
+  inputs = torch.from_numpy(file['data'][()]).unsqueeze(0).float()
+  inputs[0,0].abs_().clamp_(max=3)
+  target = None
+  try:
+    target = file['target'][0]
+  except Exception as e:
+    pass
 
   start = time.time()
   result = model.forward(inputs)
@@ -31,7 +34,7 @@ def predict_and_plot(inputs, model, target=None, timeit=False):
   if timeit:
     print('Prediction function took {:.2f} ms'.format((time.time()-start)*1000.0))
 
-  create_plot(inputs.data.cpu().numpy()[0,0], result.data.cpu().numpy()[0,0], target)
+  return create_plot(inputs.data.cpu().numpy()[0,0], result.data.cpu().numpy()[0,0], target, show=show)
 
 def isosurface(M,v,step):
     """
@@ -43,8 +46,8 @@ def isosurface(M,v,step):
     return verts, faces
     
 
-def create_plot(inputs, result, target=None):
-  f = plt.figure(figsize=(20,10))
+def create_plot(inputs, result, target=None, show=False):
+  f = plt.figure(figsize=(20,10) if show else (6,4))
   f.suptitle('Demo')
 
   ax1 = f.add_subplot(131, projection='3d')
@@ -55,7 +58,7 @@ def create_plot(inputs, result, target=None):
 
   ax2 = f.add_subplot(132, projection='3d')
   ax2.set_title('Prediction')
-  verts, faces = isosurface(inputs,1,1)
+  verts, faces = isosurface(result,1,1)
   ax2.plot_trisurf(verts[:, 0], verts[:,1], faces, verts[:, 2], lw=1, cmap="Spectral")
   ax2.view_init(elev=150, azim=-120)
 
@@ -66,22 +69,20 @@ def create_plot(inputs, result, target=None):
     ax3.plot_trisurf(verts[:, 0], verts[:,1], faces, verts[:, 2], lw=1, cmap="Spectral")
     ax3.view_init(elev=150, azim=-120)
 
-  plt.show()
+  if show:
+    plt.show()
+
+  return plt
 
 if __name__ == '__main__':
-  model = MyNet()
-  device = torch.device("cuda:0" if args.cuda else "cpu")
+  parser = argparse.ArgumentParser(description='Demo')
+  parser.add_argument('--model', type=str, default='../models/checkpoint.pth',
+                      help='Trained model path')
+  parser.add_argument('--file', type=str, default='../datasets/sample/overfit.h5',
+                      help='Use file as input')
+  parser.add_argument('--no-cuda', action='store_true', default=False,
+                      help='disables CUDA')
+  args = parser.parse_args()
+  args.cuda = not args.no_cuda and torch.cuda.is_available()
 
-  model.load_state_dict(torch.load(args.model)['state_dict'])
-  model.to(device).eval()
-
-  file = h5py.File(args.file, 'r')
-  inputs = torch.from_numpy(file['data'][()]).unsqueeze(0).float()
-  inputs[0,0].abs_().clamp_(max=3)
-  target = None
-  try:
-    target = file['target'][0]
-  except Exception as e:
-    pass
-
-  predict_and_plot(inputs, model, target)
+  main(args.model, args.file, args.cuda, show=True)

src/main.py
@@ -14,47 +14,62 @@ import utils
 
 ## SETTINGS
 parser = argparse.ArgumentParser(description='MyNet Implementation')
+# --------------- General options ---------------
 parser.add_argument('-x', '--expID', type=str, default='', metavar='S',
                     help='Experiment ID')
+parser.add_argument('--train-dir', type=str, default='../datasets/train/', metavar='S',
+                    help='folder for training files')
+parser.add_argument('--test-dir', type=str, default='../datasets/test/', metavar='S',
+                    help='folder for test files')
+parser.add_argument('--truncation', type=int, default=3, metavar='N',
+                    help='truncation value for distance field (default: 3)')
+parser.add_argument('--log-transform', type=bool, default=True, metavar='B',
+                    help='use log tranformation')
+parser.add_argument('--mask', type=bool, default=True, metavar='B',
+                    help='mask out known values')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA')
+parser.add_argument('--visdom', action='store_true', default=False,
+                    help='enables VISDOM')
+parser.add_argument('--seed', type=int, default=1, metavar='N',
+                    help='random seed (default: 1)')
+# --------------- Training options ---------------
 parser.add_argument('-b', '--batch-size', type=int, default=4, metavar='N',
                     help='input batch size for training (default: 4)')
 parser.add_argument('--test-batch-size', type=int, default=1, metavar='N',
                     help='input batch size for testing (default: 1)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
+parser.add_argument('-e', '--epochs', type=int, default=10, metavar='N',
                     help='number of epochs to train (default: 10)')
-parser.add_argument('--lr', '--learning-rate', type=float, default=1e-3, metavar='F',
-                    help='learning rate (default: 1e-3)')
-parser.add_argument('--tr', '--truncation', type=int, default=3, metavar='N',
-                    help='truncation value for distance field (default: 3)')
-parser.add_argument('--log_transform', type=bool, default=True, metavar='B',
-                    help='use log tranformation')
 parser.add_argument('--val-size', type=float, default=0.2, metavar='F',
                     help='val/(train+val) set size ratio (default: 0.2)')
+parser.add_argument('--save-interval', type=int, default=1, metavar='N',
+                    help='how many epochs to wait before saving (default: 1)')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='how many batches to wait before logging (default: 10)')
+# --------------- Optimization options ---------------
+parser.add_argument('--lr', '--learning-rate', type=float, default=1e-3, metavar='F',
+                    help='learning rate (default: 1e-3)')
+# --------------- Model options ---------------
 parser.add_argument('--model', type=str, default='', metavar='S',
                     help='use previously saved model')
 parser.add_argument('--retrain', action='store_true', default=False,
                     help='retraining from model')
-parser.add_argument('--visdom', action='store_true', default=False,
-                    help='enables VISDOM')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='disables CUDA')
-parser.add_argument('--seed', type=int, default=1, metavar='N',
-                    help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='how many batches to wait before logging (default: 10)')
 
 ## SETUP
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 args.saveDir = os.path.join('../models/', args.expID)
+utils.writeArgsFile(args,args.saveDir)
 
 if args.retrain and not args.model:
   print("\n=> No model to retraining. Double-check arguments!")
   quit()
 
+device = torch.device('cpu')
 torch.manual_seed(args.seed)
 kwargs = {}
 if args.cuda:
+  device = torch.device('cuda:0')
   torch.cuda.manual_seed_all(args.seed)
   torch.backends.cudnn.benchmark = True
   kwargs = {'num_workers': 1, 'pin_memory': True}
@@ -62,13 +77,12 @@ if args.cuda:
 if not os.path.exists(args.saveDir):
   os.makedirs(args.saveDir)
 
-utils.writeArgsFile(args,args.saveDir)
 
 ## LOAD DATASETS
 print('\nDATASET INFO.')
 
-train_data = DataHandler('../datasets/train/overfit110.h5', truncation=args.tr)
-test_data = DataHandler('../datasets/test/overfit01.h5', truncation=args.tr)
+train_data = DataHandler(args.train_dir, truncation=args.truncation)
+test_data = DataHandler(args.test_dir, truncation=args.truncation)
 
 print('Train & val. size: {} x {}'.format(len(train_data), train_data[0][0].shape))
 print('Test size: {} x {}'.format(len(test_data), test_data[0][0].shape))
@@ -77,15 +91,15 @@ print('Test size: {} x {}'.format(len(test_data), test_data[0][0].shape))
 print('\nLOADING NETWORK & SOLVER.')
 
 model = MyNet(log_transform=args.log_transform)
-device = torch.device("cuda:0" if args.cuda else "cpu")
 checkpoint = {}
 if args.model:
   checkpoint.update(torch.load(args.model))
   model.load_state_dict(checkpoint['state_dict'])
 model.to(device)
+print('Model params: {:.2f}M'.format(sum(p.numel() for p in model.parameters()) / 1e6))
 
-solver = Solver(optim_args={'lr': args.lr}, saveDir=args.saveDir,
-                vis=args.visdom)
+solver_args = {k: vars(args)[k] for k in ['saveDir', 'visdom', 'mask', 'save_interval']}
+solver = Solver(optim_args={'lr': args.lr}, args=solver_args)
 
 print('LOADED.')
 

src/nn.py
@@ -87,7 +87,9 @@ class MyNet(MyNN):
     enc3 = F.leaky_relu(self.bn3(self.enc3(enc2)))
     enc4 = F.leaky_relu(self.bn4(self.enc4(enc3)))
 
-    d1 = torch.cat([enc4,enc4], dim=1)
+    bottleneck = enc4
+
+    d1 = torch.cat([bottleneck,enc4], dim=1)
     dec1 = F.leaky_relu(self.dbn1(self.dec1(d1)))
     d2 = torch.cat([dec1,enc3], dim=1)
     dec2 = F.leaky_relu(self.dbn2(self.dec2(d2)))
@@ -97,7 +99,6 @@ class MyNet(MyNN):
     dec4 = self.dec4(d4)
 
     out = dec4
-
     if self.log_transform:
       out = out.abs().add(1).log()
 

src/solver.py
@@ -2,8 +2,9 @@ import numpy as np
 import sys
 import os
 import shutil
-
 import torch
+
+from demo import main as demo
 from utils import AverageMeter
 from viz import Viz
 
@@ -14,14 +15,15 @@ class Solver(object):
                        "weight_decay": 0.0}
 
   def __init__(self, optim=torch.optim.Adam, optim_args={},
-               loss_func=torch.nn.SmoothL1Loss(), saveDir='../models/', vis=False):
+               loss_func=torch.nn.SmoothL1Loss(), args={'saveDir':'../models/',
+               'visdom':False, 'mask':False}):
     optim_args_merged = self.default_adam_args.copy()
     optim_args_merged.update(optim_args)
     self.optim_args = optim_args_merged
     self.optim = optim
     self.loss_func = loss_func
-    self.saveDir = saveDir
-    self.visdom = Viz() if vis else False
+    self.args = args
+    self.visdom = Viz() if args['visdom'] else False
     self._reset_history()
 
   def train(self, model, train_loader, val_loader, num_epochs=10, log_nth=0,
@@ -39,7 +41,7 @@ class Solver(object):
     """
     optim = self.optim(filter(lambda p: p.requires_grad,model.parameters()),
                        **self.optim_args)
-    scheduler = torch.optim.lr_scheduler.StepLR(optim, step_size=20, gamma=.5) # False
+    scheduler = torch.optim.lr_scheduler.StepLR(optim, step_size=20, gamma=.5)
     
     iter_per_epoch = len(train_loader)
     start_epoch = 0
@@ -76,9 +78,17 @@ class Solver(object):
       train_loss = 0
 
       for i, (inputs, targets) in enumerate(train_loader, 1):
+        if model.log_transform:
+          targets = targets.abs().add(1).log()
+
         inputs, targets = inputs.to(device), targets.to(device)
+
         optim.zero_grad()
         outputs = model(inputs)
+        if self.args['mask']:
+          outputs[inputs[:,[1]] == 1] = 0
+          targets[inputs[:,[1]] == 1] = 0
+
         loss = self.loss_func(outputs, targets)
         loss.backward()
         optim.step()
@@ -93,8 +103,8 @@ class Solver(object):
                   train_loss))
 
           if self.visdom:
-            self.visdom.update_plot(x=epoch + i / iter_per_epoch,
-                                    y=train_loss,
+            x = epoch + i / iter_per_epoch
+            self.visdom.update_plot(x=x, y=train_loss,
                                     window=iter_plot,
                                     type_upd="append")
 
@@ -123,12 +133,17 @@ class Solver(object):
                                                                       val_acc,
                                                                       val_loss))
       
-      self._save_checkpoint({
-        'epoch': epoch + 1,
-        'state_dict': model.state_dict(),
-        'best_val_acc': best_val_acc,
-        'optimizer' : optim.state_dict(),
-      }, is_best)
+      # do checkpointing
+      if self.args['save_interval'] and (epoch+1) % self.args['save_interval'] == 0:
+        self._save_checkpoint({
+          'epoch': epoch + 1,
+          'state_dict': model.state_dict(),
+          'best_val_acc': best_val_acc,
+          'optimizer' : optim.state_dict(),
+        }, is_best)
+
+        if self.visdom:
+          self.visdom.matplot(demo('../models/checkpoint.pth', '../datasets/sample/overfit.h5'))
 
   def test(self, model, test_loader):
     """
@@ -148,9 +163,13 @@ class Solver(object):
         inputs, targets = inputs.to(device), targets.to(device)
         
         outputs = model.forward(inputs)
+        if self.args['mask']:
+          outputs[inputs[:,[1]] == 1] = 0
+          targets[inputs[:,[1]] == 1] = 0
+
         loss = self.loss_func(outputs, targets)
         test_loss.update(loss.item())
-        test_acc.update((outputs == targets).sum().item())
+        #test_acc.update((outputs == targets).sum().item())
 
     test_acc = float(test_acc.avg)
     test_loss = float(test_loss.avg)
@@ -161,11 +180,11 @@ class Solver(object):
     Save current state of training and trigger method to save training history.
     """
     print('Saving at checkpoint...')
-    path = os.path.join(self.saveDir, fname)
+    path = os.path.join(self.args['saveDir'], fname)
     torch.save(state, path)
     self._save_history()
     if is_best:
-      shutil.copyfile(path, os.path.join(self.saveDir, 'model_best.pth'))
+      return #shutil.copyfile(path, os.path.join(self.args['saveDir'], 'model_best.pth'))
 
   def _reset_history(self):
     """
@@ -179,7 +198,7 @@ class Solver(object):
     """
     Save training history. Conventionally the fname should end with "*.npz".
     """
-    np.savez(os.path.join(self.saveDir, fname),
+    np.savez(os.path.join(self.args['saveDir'], fname),
             train_loss_history=self.train_loss_history,
             val_loss_history=self.val_loss_history,
             val_acc_history=self.val_acc_history)
@@ -188,7 +207,7 @@ class Solver(object):
     """
     Load training history. Conventionally the fname should end with "*.npz".
     """
-    npzfile = np.load(os.path.join(self.saveDir, fname))
+    npzfile = np.load(os.path.join(self.args['saveDir'], fname))
     self.train_loss_history = npzfile['train_loss_history'].tolist()
     self.val_acc_history = npzfile['val_acc_history'].tolist()
     self.val_loss_history = npzfile['val_loss_history'].tolist()

src/viz.py
@@ -1,49 +1,26 @@
 import numpy as np
-import torch
 import visdom
-import time
 
 class Viz(object):
   def __init__(self):
     self.viz = visdom.Visdom(port=8099)
-
     # self.viz.close(None) #Close all previously
-    # self.create_notepad()
 
-  def create_plot(self, xlabel, ylabel, title, opts_dict):
+  def create_plot(self, xlabel='', ylabel='', title='', opts_dict={}):
     options = dict(xlabel=xlabel,
       ylabel=ylabel,
-      title=title + time.strftime("(%d.%m @ %H:%M:%S)"))
+      title=title)
     options.update(opts_dict)
 
-    return self.viz.line(
-      X=torch.zeros(1).cpu(),
-      Y=torch.zeros(1).cpu(),
-      opts=options)
+    return self.viz.line(X=np.array([0]),
+                         Y=np.array([0]),
+                         opts=options)
 
   def update_plot(self, x, y, window, type_upd):
-    self.viz.line(
-      X=np.array([x]),
-      Y=np.array([y]),
-      win=window,
-      update=type_upd)
+    self.viz.line(X=np.array([x]),
+                  Y=np.array([y]),
+                  win=window,
+                  update=type_upd)
 
-  def create_notepad(self):
-    txt = "This is a notepad.<br><br>"
-    cb_txt_window = self.viz.text(txt)
-
-    def type_callback(event):
-      if event['event_type'] == 'KeyPress':
-        curr_txt = event['pane_data']['content']
-        if event['key'] == 'Enter':
-          curr_txt += '<br>'
-        elif event['key'] == 'Backspace':
-          curr_txt = curr_txt[:-1]
-        elif event['key'] == 'Delete':
-          curr_txt = txt
-        elif len(event['key']) == 1:
-          curr_txt += event['key']
-        self.viz.text(curr_txt, win=cb_txt_window)
-
-    self.viz.register_event_handler(type_callback, cb_txt_window)
-
+  def matplot(self, x):
+    return self.viz.matplot(x)